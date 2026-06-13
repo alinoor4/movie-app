2526bf67bccaf791b64831f3d493aa21d900989d
alinoor4 <alinoor3865@gmail.com>
2026-06-14 00:26:11 +0500

    Added details page to show movie details, made base.html template reuseable for everything by adding it in a templates directory which is in the root directory

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='Index')
+    path('', views.index, name='movieIndex'),
+    path('<int:movieId>', views.details, name='movieDetails')
 ]
--- movies/views.py
+++ movies/views.py
@@ -6,3 +6,8 @@ from .models import Movie
 def index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
+
+
+def details(requests, movieId):
+    movie = Movie.objects.get(pk=movieId)
+    return render(requests, 'movies/details.html', {'movie': movie})
