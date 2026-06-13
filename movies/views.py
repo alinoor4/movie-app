d9ab3f3d205c13ba342b5ea0dacd63a333b248a0
alinoor4 <alinoor3865@gmail.com>
2026-06-14 00:07:04 +0500

    Added the deatils page and made the base html file accessable for everyone

--- movies/views.py
+++ movies/views.py
@@ -6,3 +6,8 @@ from .models import Movie
 def index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
+
+
+def details(request, movieId):
+    movie = Movie.objects.get(pk=movieId)
+    return render(request, 'movies/details.html', {'movie': movie})
