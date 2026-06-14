753f724060e73a3ced33f7eff32999c5eac89115
alinoor4 <alinoor3865@gmail.com>
2026-06-14 13:34:19 +0500

    Added a home page and api to the app

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .import views
+from . import views
+
 
 app_name = 'movies'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:movieId>', views.details, name='detail')
+
 ]
